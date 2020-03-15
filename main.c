0634ed3f406ed259e2f0e8a258cf9fd34b1e3688
Terrarck <56293308+Terrarck@users.noreply.github.com>
2020-03-15 14:11:19 +0100

    Question 13

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void SDL_DrawOctants(SDL_Renderer *renderer, int nCentreX,int nCentreY, int x, int y);
+void SDL_DrawCircle(SDL_Renderer * renderer, int nCentreX, int nCentreY, int nRadius);
+void SDL_DrawFilledCircle(SDL_Renderer* renderer, int nCentreX,int nCentreY, int nRadius);
+
 int main(int argc, char *argv[])
 {
     /**Variable**/
     SDL_Window *Window = NULL;/*On initialise la fenêtre*/
     SDL_Renderer* renderer;/*Déclaration du renderer*/
     SDL_Rect rect = {20,20,100,100};/*On initialise un rectangle (position X, position Y, largeur, hauteur)*/
-    SDL_Point point = {20,40};/*On initialise point position X, position Y*/
+    //SDL_Point point = {20,40};/*On initialise point position X, position Y*/
 
     /**Initialisation**/
     if(0 != SDL_Init(SDL_INIT_EVERYTHING))/* initialisation*/
@@ -18,7 +22,7 @@ int main(int argc, char *argv[])
     }
     /**Ouverture de la fenêtre**/
     Window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              300,300, SDL_WINDOW_SHOWN);
+                              400,400, SDL_WINDOW_SHOWN);
 
     /**Fenetre Error**/
     if(NULL == Window)
@@ -46,6 +50,24 @@ int main(int argc, char *argv[])
     /* Affiche la couleur présente */
         SDL_RenderPresent(renderer);
 
+
+        SDL_SetRenderDrawColor(renderer,0,0,0,255);
+        SDL_RenderDrawRect(renderer,&rect);/*On dessine le rectangle*/
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000);
+
+        SDL_RenderFillRect(renderer,&rect); /*Change la couleur*/
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000);
+
+        SDL_DrawCircle(renderer,200,100,40);/*On dessine le cercle*/
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000);
+
+        SDL_DrawFilledCircle(renderer,200,200,40);/*On remplit de couleur*/
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1000);
+
     getchar();//Attendre une validation pour fermer la fenêtre
     SDL_DestroyRenderer(renderer); // Destruction du renderer et de la fenêtre :
     SDL_DestroyWindow(Window); //Supprime la fenêtre*
@@ -53,5 +75,72 @@ int main(int argc, char *argv[])
     /**Quitter**/
     SDL_Quit();
     return EXIT_SUCCESS;
+}
+
+/** octants de l'algorime de Bresenhams pour le circle**/
+void SDL_DrawOctants(SDL_Renderer *renderer, int nCentreX,int nCentreY, int x, int y) {
+    SDL_RenderDrawPoint(renderer,nCentreX+x,nCentreY+y);
+    SDL_RenderDrawPoint(renderer,nCentreX-x,nCentreY+y);
+    SDL_RenderDrawPoint(renderer,nCentreX+x,nCentreY-y);
+    SDL_RenderDrawPoint(renderer,nCentreX-x,nCentreY-y);
+    SDL_RenderDrawPoint(renderer,nCentreX+y,nCentreY+x);
+    SDL_RenderDrawPoint(renderer,nCentreX-y,nCentreY+x);
+    SDL_RenderDrawPoint(renderer,nCentreX+y,nCentreY-x);
+    SDL_RenderDrawPoint(renderer,nCentreX-y,nCentreY-x);
+}
+
+/**Trace un cercle vide**/
+void SDL_DrawCircle(SDL_Renderer * renderer, int nCentreX, int nCentreY, int nRadius) {
+    int x = 0;
+    int y = 0;
+    int d;
+    d = 3 - 2*nRadius;
+    y = nRadius;
+
+    SDL_DrawOctants(renderer,nCentreX,nCentreY,x,y);
+    while (y >= x) {
+        x++;
+        if (d > 0) {
+            y--;
+            d = d +  4*(x-y)+10;
+        } else {
+            d = d + 4*x + 6;
+        }
+        SDL_DrawOctants(renderer,nCentreX,nCentreY,x,y);
+    }
+}
+
+/**Trace un cercle plein**/
+void SDL_DrawFilledCircle(SDL_Renderer* renderer, int nCentreX,int nCentreY, int nRadius){
+    int x = 0;
+    int y = 0;
+    int d;
+    d = 3 - 2*nRadius;
+    y = nRadius;
+
+    SDL_DrawOctants(renderer,nCentreX,nCentreY,x,y);
+
+    //Lignes horizontales entre les octants
+    SDL_RenderDrawLine(renderer,nCentreX - x,nCentreY - y, nCentreX + x,nCentreY - y);
+    SDL_RenderDrawLine(renderer,nCentreX - y,nCentreY + x, nCentreX + y,nCentreY + x);
+    SDL_RenderDrawLine(renderer,nCentreX - y,nCentreY - x, nCentreX + y,nCentreY - x);
+    SDL_RenderDrawLine(renderer,nCentreX - x,nCentreY + y, nCentreX + x,nCentreY + y);
+
+    while (y >= x) {
+        x++;
+        if (d > 0) {
+            y--;
+            d = d +  4*(x-y)+10;
+        } else {
+            d = d + 4*x + 6;
+        }
+        SDL_DrawOctants(renderer,nCentreX,nCentreY,x,y);
+
+        //Lignes horizontales entre les octants
+        SDL_RenderDrawLine(renderer,nCentreX - x,nCentreY - y, nCentreX + x,nCentreY - y);
+        SDL_RenderDrawLine(renderer,nCentreX - y,nCentreY + x, nCentreX + y,nCentreY + x);
+        SDL_RenderDrawLine(renderer,nCentreX - y,nCentreY - x, nCentreX + y,nCentreY - x);
+        SDL_RenderDrawLine(renderer,nCentreX - x,nCentreY + y, nCentreX + x,nCentreY + y);
+    }
 }
 
