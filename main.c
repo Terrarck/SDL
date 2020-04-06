0ddb06b87b5f6541776377dd60acf75128ae0f6c
Terrarck <56293308+Terrarck@users.noreply.github.com>
2020-04-06 10:44:08 +0100

    Question 5

--- main.c
+++ main.c
@@ -10,11 +10,14 @@ int main(int argc, char *argv[])
     SDL_Texture *pTexture=NULL;
     SDL_Surface *image = NULL;
 
-    SDL_Rect rect = {0,0,240,400};
+    SDL_Rect rect = {0,0,500,500},src= {0,0,8,8};
 
-    pWindow = SDL_CreateWindow("SDL2",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_RESIZABLE);
+    SDL_SetRenderDrawColor(pRenderer,255,255,255,1);
+    SDL_RenderClear(pRenderer);
 
-    image = IMG_Load("image.png");
+    pWindow = SDL_CreateWindow("SDL2",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WINDOW_RESIZABLE);
+
+    image = IMG_Load("image2.png");
 
     if (pWindow == NULL)
     {
@@ -29,18 +32,19 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    pTexture = SDL_CreateTextureFromSurface(pRenderer,image);
 
-
-        pTexture = SDL_CreateTextureFromSurface(pRenderer,image);
-
-        SDL_RenderCopy(pRenderer,pTexture,NULL,&rect);
+    for (int i=0; i<20; i++)
+    {
+        src.x = i*8;
+        SDL_RenderCopy(pRenderer,pTexture,&src,&rect);
 
         SDL_RenderPresent(pRenderer);
 
-        SDL_Delay(2500);
-
-        SDL_Quit();
-        return 0;
-
+        SDL_Delay(1000);
     }
 
+    SDL_Quit();
+    return 0;
+
+}
